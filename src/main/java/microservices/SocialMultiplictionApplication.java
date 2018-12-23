ee0d2122d03886b6a32c92dd70d40e88bbb92dbf
Brian Velez <bvez94@gmail.com>
2018-12-23 13:13:18 -0800

    Updated main directory structure.

--- src/main/java/microservices/SocialMultiplictionApplication.java
+++ src/main/java/microservices/SocialMultiplictionApplication.java
@@ -1,4 +1,4 @@
-package microservices.multiplication;
+package microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
